b8adbb08400fefed4996aa0c0f6dcee835587e9b
Alexander Jansen <126659284+ajansen95@users.noreply.github.com>
2023-07-27 15:02:36 +0200

    cleanup

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import { Server } from "socket.io";
 
 const io = new Server(3001,{
     cors: {
-        origin: "http://localhost:5173",
-    },
-});
+        origin: "http://localhost:5173"
+    }
+})
 
 
 io.on('connection', (socket) => {
