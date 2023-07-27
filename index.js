6b2434c2c43524f4f910d348eb7bf8a2db0819f9
Alexander Jansen <126659284+ajansen95@users.noreply.github.com>
2023-07-27 14:59:33 +0200

    plain websockets connect/disconnect

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-import express from 'express'
-const app = express()
-import * as http from 'http'
-const server = http.createServer(app)
-
 import { Server } from "socket.io";
-const io = new Server(server,{
+
+
+const io = new Server(3001,{
     cors: {
         origin: "http://localhost:5173",
     },
 });
 
 
-app.get('/', (req, res) => {
-    res.send('<h1>vite-typescript-webrtc-backend</h1>');
-});
-
 io.on('connection', (socket) => {
-    console.log('a user connected');
-});
-
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+    console.log(socket.id + " connected")
+    socket.on('disconnect', () => console.log(socket.id + " disconnected"))
 });
