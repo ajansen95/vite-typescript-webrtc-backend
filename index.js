d30d6d7b5515560947c6dd41263d1c9101794d1a
Alexander Jansen <126659284+ajansen95@users.noreply.github.com>
2023-08-01 17:47:51 +0200

    stream works sometimes (e.g. when refreshed from console)

--- index.js
+++ index.js
@@ -10,5 +10,38 @@ const io = new Server(3001,{
 
 io.on('connection', (socket) => {
     console.log(socket.id + " connected")
-    socket.on('disconnect', () => console.log(socket.id + " disconnected"))
+    sendClientListToEveryone(socket)
+    socket.on('disconnect', () => {
+        console.log(socket.id + " disconnected")
+        sendClientListToEveryone(socket)
+    })
+
+    socket.on("offer", (offer) => {
+        console.log(offer)
+        socket.to(offer.toID).emit("offer", {
+            fromID: socket.id,
+            sdp: offer.sdp
+        })
+    })
+
+    socket.on("answer", (answer) => {
+        console.log(answer)
+        socket.to(answer.toID).emit("answer", {
+            fromID: socket.id,
+            sdp: answer.sdp
+        })
+    })
+
+    socket.on("new-ice-candidate", (ice) => {
+        socket.to(ice.toID).emit("new-ice-candidate", ice)
+    })
 });
+
+
+function sendClientListToEveryone(socket) {
+    let clientList = []
+    io.of("/").sockets.forEach(client => clientList.push(client.id))
+    socket.broadcast.emit("list", clientList)
+    socket.emit("list", clientList)
+}
+
